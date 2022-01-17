030a5c910d210ce849bbde23aefd8b944f6658b6
MarsaHawari <68333057+MarsaHawari@users.noreply.github.com>
2022-01-17 20:38:48 +0700

    Laravel-Resto

---
+++ laravel-resto/database/factories/KategoriFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Kategori;
+
+class KategoriFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     * 
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'kategori' => $this->faker->name(),
+        ];
+    }
+}
